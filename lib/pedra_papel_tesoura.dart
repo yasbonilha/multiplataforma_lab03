b0d5650e27486dcd37272563a73e50c2056c81b1
yasmin bonilha <bonilhayaya@gmail.com>
2024-08-20 14:12:35 -0300

    lab03(dart): desafio completo

--- lib/pedra_papel_tesoura.dart
+++ lib/pedra_papel_tesoura.dart
@@ -5,6 +5,10 @@ import "dart:math";
 //definir um enum com as opções de jogo (pedra, papel, tesoura, sair) - o enum fica fora da função
 enum OPCOES {pedra, papel, tesoura, sair}
 
+int pontosComputador = 0;
+int pontosUsuario = 0;
+String placar = "jogador: $pontosUsuario x computador: $pontosComputador";
+
 void exibir(String texto){
   print(texto);
 }
@@ -32,20 +36,37 @@ OPCOES mapearOpcao( int opcao) {
 String verificaResultado(OPCOES opUsuario, OPCOES opComputador){
   if(opUsuario == opComputador) return "empate";
   if(opUsuario == OPCOES.pedra){
-    return opComputador == OPCOES.papel? "o computador venceu." : "você venceu.";
-  } else if(opUsuario == OPCOES.papel){
-    // return opComputador == OPCOES.pedra? "você venceu." : "o computador venceu.";
-    // tammbém temos a opção de fazer com o switch expressional: 
-    return switch(opComputador){
-      OPCOES.tesoura => "o computador venceu.",
-      _ => "você venceu."
-    };
-  }
-  else{
-    if(opComputador == OPCOES.pedra){
+    // return opComputador == OPCOES.papel? "o computador venceu." : "você venceu.";
+    if(opComputador == OPCOES.papel){
+      pontosComputador++;
       return "o computador venceu.";
     }
     else{
+      pontosUsuario++;
+      return "você venceu.";
+    }
+  } else if(opUsuario == OPCOES.papel){
+    // return opComputador == OPCOES.pedra? "você venceu." : "o computador venceu.";
+    // tammbém temos a opção de fazer com o switch expressional: 
+    // return switch(opComputador){
+    //   OPCOES.tesoura => "o computador venceu.",
+    //   _ => "você venceu."
+    // };
+    if(opComputador == OPCOES.tesoura){
+      pontosComputador++;
+      return "o computador venceu.";
+    } else{
+      pontosUsuario++;
+      return "você venceu.";
+    }
+  }
+  else{
+    if(opComputador == OPCOES.pedra){
+      pontosComputador++;
+      return "o computador venceu.";
+    }
+    else{
+      pontosUsuario++;
       return "você venceu.";
     }
 
@@ -55,21 +76,25 @@ String verificaResultado(OPCOES opUsuario, OPCOES opComputador){
 
 void jogo() {
 int opUsuario;
+exibir('olá jogador. quantas rodadas deseja jogar?');
 int rodadas = pegarRodadas();
-do{
-  while()
+while(!rodadaValida(rodadas)){
+  exibir("o número de rodadas selecionadas não é válido. por favor, escolha um número apropriado.");
+  rodadas = pegarRodadas();
+}
+while(rodadaValida(rodadas)){
     do{
-      var menu = "1 - pedra \n2 - papel \n3 - tesoura \n4 - sair";
+      var menu = "1 - pedra \n2 - papel \n3 - tesoura";
       exibir(menu);
 
       opUsuario = pegarOpcaoUsuario();
 
     }while(!opcaoValida(opUsuario));
 
-    if(opUsuario == 4) {
-      exibir("até logo");
-    }
-    else{
+    // if(opUsuario == 4) {
+    //   exibir("até logo");
+    // }
+    // else{
       var opComputador = Random().nextInt(3) + 1; //aqui, apesar de já termos declarado a variável, o compilador nao sabe o valor que dará (porque é random), entao nao dá para ser constante
       OPCOES opcaoUsuario = mapearOpcao(opUsuario);
       final opcaoComputador = mapearOpcao(opComputador);
@@ -77,10 +102,11 @@ do{
       exibir('JOGADOR(${opcaoUsuario.name }) vs (${opcaoComputador.name})COMPUTADOR');
       final resultado = verificaResultado(opcaoUsuario, opcaoComputador);
       exibir(resultado);
+      exibir(placar);
 
-    }
-
+    // }
+  rodadas--;
 }
-while(rodadaValida(rodadas));
-
+exibir("placar final: $placar");
+exibir("o número de partidas selecionado acabou. até logo.");
 } //essa função está sendo executada lá na função principal (main), que está na pasta bin
